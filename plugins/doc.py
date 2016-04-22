5e323ad79d6b9b8a4a9ddbf53c6fa82896f0df8c
Stephen Hosom <0xhosom@gmail.com>
2016-04-22 09:47:05 -0400

    updates to the document analyzer

--- plugins/doc.py
+++ plugins/doc.py
@@ -1,4 +1,5 @@
 from oletools import olevba
+from oletools.thirdparty import olefile
 
 class Plugin:
 
@@ -18,13 +19,38 @@ class Plugin:
 		self.suspicious_on_macro = True
 
 	def analyze(self, afile):
+		'''Analyze OLE files and extract metadata about the file into the 
+		FileAnalysis object.
+
+		Args:
+			afile (FileAnalysis): The file to be analyzed.
 		
+		Returns:
+			None
+		'''
 		if afile.mime_type in self.analyzed_mimes:
+
+			try:
+				ole = olefile.OleFileIO(afile.path)
+			except IOError:
+				afile.errors = afile.errors + ['doc plugin: unsupported filetype']
+				output = 'None'
+				afile.plugin_output[self.__NAME__] = output
+
+			meta = ole.get_metadata()
+
+			for prop in meta.SUMMARY_ATTRIBS:
+				value = getattr(meta, prop)
+				setattr(afile, prop, value)
+
+			for prop in meta.DOCSUM_ATTRIBS:
+				value = getattr(meta, prop)
+				setattr(afile, prop, value)
+
 			try:
 				parser = olevba.VBA_Parser(afile.path)
 			except TypeError:
 				afile.errors = afile.errors + ['doc plugin: unsupported filetype']
-				#afile.errors = afile.errors + ['TypeError']
 				output = 'None'
 				afile.plugin_output[self.__NAME__] = output
 				return
