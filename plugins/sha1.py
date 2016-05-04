a9d2ec99265e023c6968d2e33e1bc58b53942669
Stephen Hosom <0xhosom@gmail.com>
2016-05-04 08:45:08 -0400

    update sha1 plugin

--- plugins/sha1.py
+++ plugins/sha1.py
@@ -6,12 +6,20 @@ class Plugin:
 
 	def __init__(self, args):
 
-		self.chunk_size = args.get('chunk_size')
-		if self.chunk_size == None:
+		try:
+			self.chunk_size = int(args.get('chunk_size'))
+		except TypeError, ValueError:
 			self.chunk_size = 4096
 
 	def analyze(self, afile):
+		'''Hash the analyzed file. 
 
+		Args:
+			afile (FileAnalysis): The file to be analyzed.
+		
+		Returns:
+			None
+		'''
 		hasher = sha1()
 		with open(afile.path, 'rb') as f:
 			for chunk in iter(lambda: f.read(self.chunk_size), b""):
