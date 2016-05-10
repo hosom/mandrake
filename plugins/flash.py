f7efcb4f20640605cf717209615bbdf5dad861ef
Zack Nagaich <zacknagaich@gmail.com>
2016-05-10 09:59:50 -0400

    fixed regex search none check, added comments

--- plugins/flash.py
+++ plugins/flash.py
@@ -42,17 +42,21 @@ class Plugin:
 						setattr(afile,'hasMetadata',tag.hasMetadata)
 						setattr(afile,'actionscript3',tag.actionscript3)
 						setattr(afile,'useNetwork',tag.useNetwork)
+
 					elif tag.name == 'Metadata':
+						# Iterate through xml tags and pass to regex search to easily pull out value. 
 						xml_tags = ['format','title','description','publisher','creator','language','date']
 						for xt in xml_tags:
 							search = re.search(r'<dc:'+xt+'>(.*)</dc:'+xt+'>',tag.xmlString,re.M|re.I)
 							if search:
 								setattr(afile,xt,search.group(1))
-							if xt == 'title' and search:
-								afile.plugin_output[self.__NAME__] = search.group(1)
+								if xt == 'title':
+									afile.plugin_output[self.__NAME__] = search.group(1)
+
 					elif tag.name == 'TagScriptLimits':
 						setattr(afile,'MaxRecursionDepth',tag.maxRecursionDepth)
 						setattr(afile,'ScriptTimeout',tag.scriptTimeoutSeconds)
+
 					elif tag.name == 'TagExportAssets':
 						setattr(afile,'Exports',tag.exports)
  
